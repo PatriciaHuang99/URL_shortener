88330869034739a92e8645661dde3a9756ee6a41
Andy Hsu <ssarcandy@gmail.com>
2023-08-06 01:12:28 +0800

    de

--- index.js
+++ index.js
@@ -4,32 +4,32 @@
  */
 
 const express = require("express");
-const res = require("express/lib/response");
-const mongoose = require("mongoose"); 
-const app = express();
-const ShortUrl = require("./models/shortUrl");
+// const res = require("express/lib/response");
+// const mongoose = require("mongoose"); 
+// const app = express();
+// const ShortUrl = require("./models/shortUrl");
 
-mongoose
-  // DB_URL is defined in docker-compose.yml.
-  .connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected!"));
+// mongoose
+//   // DB_URL is defined in docker-compose.yml.
+//   .connect(process.env.DB_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Connected!"));
   
-mongoose.connection.on("error", (error) => {
-   console.log("Error connecting to MongoDB:", error);
- });
+// mongoose.connection.on("error", (error) => {
+//    console.log("Error connecting to MongoDB:", error);
+//  });
 
- mongoose.connection.on("disconnected", () => {
-   console.log("Disconnected from MongoDB");
- });
+//  mongoose.connection.on("disconnected", () => {
+//    console.log("Disconnected from MongoDB");
+//  });
 
-app.set("views", "views");
-app.set("view engine", "ejs"); 
+// app.set("views", "views");
+// app.set("view engine", "ejs"); 
 
-// Middleware to parse URL-encoded form data and reserve special characters for sending data
-app.use(express.urlencoded({ extended: false }));
+// // Middleware to parse URL-encoded form data and reserve special characters for sending data
+// app.use(express.urlencoded({ extended: false }));
 
 // Render the index view with the shortUrls object.
 app.get("/", async (req, res) => {
@@ -41,20 +41,20 @@ app.get("/", async (req, res) => {
 });
 
 // Create a new ShortUrl object and redirect to the index view.
-app.post("/shortUrls", async (req, res) => {
-  // Create a new ShortUrl Obj in the database with the provided full URL from the request body
-  await ShortUrl.create({ full: req.body.fullURL });
-  res.redirect("/");
-});
+// app.post("/shortUrls", async (req, res) => {
+//   // Create a new ShortUrl Obj in the database with the provided full URL from the request body
+//   await ShortUrl.create({ full: req.body.fullURL });
+//   res.redirect("/");
+// });
 
-// Find the record in ShortUrl that has the same short name as the URL.
-app.get("/:shortUrl", async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
-  if (shortUrl == null) return res.sendStatus(404);
-  shortUrl.clicks++;
-  shortUrl.save();
-  res.redirect(shortUrl.full);
-});
+// // Find the record in ShortUrl that has the same short name as the URL.
+// app.get("/:shortUrl", async (req, res) => {
+//   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+//   if (shortUrl == null) return res.sendStatus(404);
+//   shortUrl.clicks++;
+//   shortUrl.save();
+//   res.redirect(shortUrl.full);
+// });
 
 app.listen(process.env.PORT || 3000);
 module.exports = app
