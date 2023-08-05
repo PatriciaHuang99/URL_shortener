9599de967593dde506c963666b35ffb7fa51f186
Andy Hsu <ssarcandy@gmail.com>
2023-08-06 01:17:34 +0800

    vdftg

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ app.use(express.urlencoded({ extended: false }));
 // Render the index view with the shortUrls object.
 app.get("/", async (req, res) => {
   const shortUrls = [] ;//await ShortUrl.find();
-  res.render("index", { shortUrls: shortUrls });
+  // res.render("index", { shortUrls: shortUrls });
+  res.sen("Hi")
 });
 
 // Create a new ShortUrl object and redirect to the index view.
